b5dc4d8e29532dd3bae2fbdcb2bf4a1e28475c50
laine <lainepos@gmail.com>
2025-10-29 23:01:26 -0300

    Adicionando Screen

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,21 +1,27 @@
 describe('Login', () => {
+  beforeEach(() => {
+     cy.visit('http://localhost:4000')
+  })
+
   it('Validar login com dados válidos para permitir acesso a aplicação escala ministerial', () => {
-    cy.visit('http://localhost:4000')
+   
     cy.get(':nth-child(2) > input').click().type('leader1')
     cy.get(':nth-child(3) > input').click().type('password')
     cy.get('#login-button').click()
+    cy.screenshot('apos-login-com-dados-validos')
 
     //Assert valida que o login foi efetuado com sucesso
     cy.contains('Escala Ministerial').should('be.visible')
   })
 
     it('Validar login com dados inválidos deve permitir mensagem de erro', () => {
-    cy.visit('http://localhost:4000')
+    
     cy.get(':nth-child(2) > input').click().type('leader1')
     cy.get(':nth-child(3) > input').click().type('2345')
     cy.get('#login-button').click()
 
     //Assert valida que o login foi efetuado com sucesso
     cy.get('#login-error').should('have.text', '{"message":"Um erro ocorreu"}')
+    cy.screenshot('apos-login-com-dados-invalidos')
   })
 })
