948320fb2e387b15e6d5cd3803d9087381558488
laine <lainepos@gmail.com>
2025-10-29 22:36:52 -0300

    Adicionando novo teste de login invalido

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,4 +8,14 @@ describe('Login', () => {
     //Assert valida que o login foi efetuado com sucesso
     cy.contains('Escala Ministerial').should('be.visible')
   })
+
+    it('Validar login com dados inválidos deve permitir mensagem de erro', () => {
+    cy.visit('http://localhost:4000')
+    cy.get(':nth-child(2) > input').click().type('leader1')
+    cy.get(':nth-child(3) > input').click().type('2345')
+    cy.get('#login-button').click()
+
+    //Assert valida que o login foi efetuado com sucesso
+    cy.get('#login-error').should('have.text', '{"message":"Um erro ocorreu"}')
+  })
 })
